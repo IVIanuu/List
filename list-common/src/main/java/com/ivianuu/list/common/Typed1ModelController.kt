bf2819b8ab70d2a1d411dc6b055aad0b775105af
IVIanuu <IVIanuu@gmail.com>
2019-03-12 10:52:01 +0100

    Added typed model controller

---
+++ list-common/src/main/java/com/ivianuu/list/common/Typed1ModelController.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.list.common
+
+import com.ivianuu.list.ListPlugins
+import com.ivianuu.list.ModelController
+import com.ivianuu.list.defaultBuildingExecutor
+import com.ivianuu.list.defaultDiffingExecutor
+import java.util.concurrent.Executor
+
+/**
+ * @author Manuel Wrage (IVIanuu)
+ */
+abstract class Typed1ModelController<A>(
+    diffingExecutor: Executor = ListPlugins.defaultDiffingExecutor,
+    buildingExecutor: Executor = ListPlugins.defaultBuildingExecutor
+) : ModelController(diffingExecutor, buildingExecutor) {
+
+    var data1: A? = null
+        set(value) {
+            field = value
+            allowModelBuildRequests = true
+            requestModelBuild()
+            allowModelBuildRequests = false
+        }
+
+    private var allowModelBuildRequests = false
+
+    override fun requestModelBuild() {
+        if (!allowModelBuildRequests) {
+            throw IllegalStateException("cannot be called directly use setData() instead")
+        }
+
+        super.requestModelBuild()
+    }
+
+    final override fun buildModels() {
+        if (!isBuildingModels) {
+            throw IllegalStateException("cannot be called directly use setData() instead")
+        }
+        buildModels(data1!!)
+    }
+
+    protected abstract fun buildModels(data1: A)
+
+}
+
+class SimpleTyped1ModelController<A>(
+    private val buildModels: (A) -> Unit
+) : Typed1ModelController<A>() {
+    override fun buildModels(data1: A) {
+        buildModels.invoke(data1)
+    }
+}
+
+fun <A> typed1Controller(buildModels: (A) -> Unit) =
+    SimpleTyped1ModelController(buildModels = buildModels)
--- sample/src/main/java/com/ivianuu/list/sample/MainActivity.kt
+++ sample/src/main/java/com/ivianuu/list/sample/MainActivity.kt
@@ -1,35 +1,42 @@
 package com.ivianuu.list.sample
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ivianuu.list.ListModel
 import com.ivianuu.list.annotations.Model
 import com.ivianuu.list.common.LayoutContainerHolder
 import com.ivianuu.list.common.LayoutContainerModel
+import com.ivianuu.list.common.ModelTouchHelper
 import com.ivianuu.list.common.controller
 import com.ivianuu.list.common.onClick
 import com.ivianuu.list.id
 import kotlinx.android.synthetic.main.activity_main.list
 import kotlinx.android.synthetic.main.item_simple.title
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val models = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        models.addAll((1..100).map { "Title: $it" })
+
         list.layoutManager = LinearLayoutManager(this)
 
         val controller = controller {
-            (1..100)
-                .map { i ->
+            models.forEach {
                     simple {
-                        id(i)
-                        text("Title: $i")
+                        id(it)
+                        text(it)
                         onClick { _, _ ->
                             Toast.makeText(
-                                this@MainActivity, "Clicked model at $i",
+                                this@MainActivity, "Clicked model at $it",
                                 Toast.LENGTH_SHORT
                             ).show()
                         }
@@ -40,6 +47,24 @@ class MainActivity : AppCompatActivity() {
         controller.requestModelBuild()
 
         list.adapter = controller.adapter
+
+        ModelTouchHelper.dragging(list)
+            .vertical()
+            .all()
+            .callbacks(
+                object : ModelTouchHelper.DragCallbacks<ListModel<*>>() {
+                    override fun onModelMoved(
+                        fromPosition: Int,
+                        toPosition: Int,
+                        modelBeingMoved: ListModel<*>,
+                        itemView: View
+                    ) {
+                        super.onModelMoved(fromPosition, toPosition, modelBeingMoved, itemView)
+                        Collections.swap(models, fromPosition, toPosition)
+                        controller.requestImmediateModelBuild()
+                    }
+                }
+            )
     }
 }
 
