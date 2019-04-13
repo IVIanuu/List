2f974bc1ff32de0f55a054a9cb8f2758a0d4174c
IVIanuu <IVIanuu@gmail.com>
2019-04-13 18:52:24 +0200

    Updated deps

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -14,7 +14,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.list"
     const val vcsUrl = "https://github.com/IVIanuu/list"
-    const val version = "${Build.versionName}-dev-24"
+    const val version = "${Build.versionName}-dev-25"
 }
 
 object Versions {
